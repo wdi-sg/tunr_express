3b6ac7b2b1741cd5300bb377234a010627c020fa
claraonmars <claraonmars@hotmail.com>
2018-10-03 03:20:13 +0800

    Build index feature

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const express = require('express');
 const methodOverride = require('method-override');
 const pg = require('pg');
 
+
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
-  host: '127.0.0.1',
-  database: 'tunr_db',
-  port: 5432,
+    user: 'mac',
+    host: '127.0.0.1',
+    database: 'tunr_db',
+    port: 5432,
 };
 
 const pool = new pg.Pool(configs);
 
-pool.on('error', function (err) {
-  console.log('idle client error', err.message, err.stack);
+pool.on('error', function(err) {
+    console.log('idle client error', err.message, err.stack);
 });
 
 /**
@@ -27,10 +28,11 @@ pool.on('error', function (err) {
 // Init express app
 const app = express();
 
+app.use(express.static('public'))
 
 app.use(express.json());
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }));
 
 app.use(methodOverride('_method'));
@@ -49,16 +51,20 @@ app.engine('jsx', reactEngine);
  */
 
 app.get('/', (req, res) => {
-  // query database for all pokemon
+    // respond with HTML page displaying all artist
+    let sqlText = 'SELECT * FROM artists';
 
-  // respond with HTML page displaying all pokemon
-  response.render('home');
+    pool.query(sqlText, (error, queryResults) => {
+        if (error) {
+            console.log('error!', error);
+            res.status(500).send('DIDNT WORKS!!');
+        } else {
+            res.render('home', { artists: queryResults.rows });
+        }
+    });
 });
 
-app.get('/new', (request, response) => {
-  // respond with HTML page with form to create new pokemon
-  response.render('new');
-});
+
 
 
 /**
@@ -68,10 +74,10 @@ app.get('/new', (request, response) => {
  */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
-server.on('close', () => {
-  console.log('Closed express server');
+// server.on('close', () => {
+//   console.log('Closed express server');
 
-  db.pool.end(() => {
-    console.log('Shut down db connection pool');
-  });
-});
+//   db.pool.end(() => {
+//     console.log('Shut down db connection pool');
+//   });
+// });
--- views/home.jsx
+++ views/home.jsx
@@ -1,14 +1,29 @@
 var React = require("react");
+var DefaultLayout = require('./layout/default');
+
 
 class Home extends React.Component {
   render() {
+
+    const artistElements = this.props.artists.map((artist)=>{
+
+        let linkPath = "/artist/"+artist.id;
+
+        return (<li><a href={linkPath}>{artist.name}</a></li>);
+
+    });
+
     return (
-      <html>
-        <head />
-        <body>
-          <h1>Welcome!</h1>
-        </body>
-      </html>
+                <DefaultLayout title="New Task">
+
+            <h1>All Artists</h1>
+            <ul>
+            {artistElements}
+            </ul>
+
+            <button><a href="/new">Add new artist page</a></button>
+
+          </DefaultLayout>
     );
   }
 }
