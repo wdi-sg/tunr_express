680b0013723bc77bfe27a2d1b2e1027b81e685f3
shroomcrumpet <kencheng3@gmail.com>
2018-10-02 20:38:00 +0800

    Part 1 complete: create db, tables, seed tables, set up root get

--- artist_data.sql
+++ artist_data.sql
@@ -1,5 +1,10 @@
-INSERT INTO artists(name, photo_url, nationality) VALUES('Yeah Yeah Yeahs', 'http://www.athousandguitars.com/wp-content/uploads/2013/04/yeah-yeah-yeahs.jpg', 'USA');
+
+
+INSERT INTO artists(name, photo_url, nationality) VALUES('Yeah Yeah Yeahs', 'https://consequenceofsound.files.wordpress.com/2017/10/yeah-yeah-yeahs.png', 'USA');
 INSERT INTO artists(name, photo_url, nationality) VALUES('Nosaj Thing', 'http://wertn.com/wp-content/uploads/2012/04/Nosaj-Thing_Mondrian_CL_High-3487.jpg', 'USA');
 INSERT INTO artists(name, photo_url, nationality) VALUES('Norah Jones', 'http://entertainmentrealm.files.wordpress.com/2012/05/norahjones1.jpg', 'USA');
-INSERT INTO artists(name, photo_url, nationality) VALUES('Lykke Li', 'http://www.chartattack.com/wp-content/uploads/2012/07/lykke-li-newmain1-photo-by-daniel-jackson.jpg', 'Sweeden');
-INSERT INTO artists(name, photo_url, nationality) VALUES('Kendrick Lamar', 'http://www.xxlmag.com/wp-content/uploads/2013/06/kendricklamar_001-1600.jpg', 'USA');
+INSERT INTO artists(name, photo_url, nationality) VALUES('Lykke Li', 'https://ksassets.timeincuk.net/wp/uploads/sites/55/2015/03/2014WarChild_TomOldham_LykkeLi01f2271014.jpg', 'Sweden');
+INSERT INTO artists(name, photo_url, nationality) VALUES('Kendrick Lamar', 'https://www.grammy.com/sites/com/files/kendricklamar-spotlight.png', 'USA');
+
+
+--run from Terminal: psql -d tunr_db -U kencheng -f artist_data.sql
--- index.js
+++ index.js
@@ -1,46 +1,39 @@
-console.log("starting up!!");
+console.log("Starting up!");
 
 const express = require('express');
 const methodOverride = require('method-override');
 const pg = require('pg');
 
-// Initialise postgres client
+
 const configs = {
-  user: 'YOURUSERNAME',
-  host: '127.0.0.1',
-  database: 'tunr_db',
-  port: 5432,
+    user: 'kencheng',
+    host: '127.0.0.1',
+    database: 'tunr_db',
+    port: 5432,
 };
 
 const pool = new pg.Pool(configs);
 
-pool.on('error', function (err) {
-  console.log('idle client error', err.message, err.stack);
+pool.on('error', function(err) {
+    console.log('idle client error', err.message, err.stack);
 });
 
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
 
-// Init express app
 const app = express();
-
 
 app.use(express.json());
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }));
 
 app.use(methodOverride('_method'));
 
 
-// Set react-views to be the default view engine
 const reactEngine = require('express-react-views').createEngine();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', reactEngine);
+
 
 /**
  * ===================================
@@ -48,16 +41,17 @@ app.engine('jsx', reactEngine);
  * ===================================
  */
 
-app.get('/', (req, res) => {
-  // query database for all pokemon
 
-  // respond with HTML page displaying all pokemon
-  response.render('home');
+app.get('/', (req, response) => {
+    // query database for all pokemon
+
+    // respond with HTML page displaying all pokemon
+    response.render('root');
 });
 
 app.get('/new', (request, response) => {
-  // respond with HTML page with form to create new pokemon
-  response.render('new');
+    // respond with HTML page with form to create new pokemon
+    response.render('new');
 });
 
 
@@ -66,12 +60,14 @@ app.get('/new', (request, response) => {
  * Listen to requests on port 3000
  * ===================================
  */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+
+const server = app.listen(3001, () => console.log('~~~ Tuning in to the waves of port 3001 ~~~'));
 
 server.on('close', () => {
-  console.log('Closed express server');
+    console.log('Closed express server');
 
-  db.pool.end(() => {
-    console.log('Shut down db connection pool');
-  });
+    db.pool.end(() => {
+        console.log('Shut down db connection pool');
+    });
 });
