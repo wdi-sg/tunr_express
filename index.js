a164b9e1d1a7a9dee25a995a53605407e8c7c22f
andrealmj <andrealmj@live.com>
2019-01-09 15:07:00 +0800

    set up express app

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const pg = require('pg');
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
+  user: 'andrealmj',
   host: '127.0.0.1',
   database: 'tunr_db',
   port: 5432,
+  password: 'pg'
 };
 
 const pool = new pg.Pool(configs);
@@ -48,11 +49,8 @@ app.engine('jsx', reactEngine);
  * ===================================
  */
 
-app.get('/', (req, res) => {
-  // query database for all pokemon
-
-  // respond with HTML page displaying all pokemon
-  response.render('home');
+app.get('/', (request, response) => {
+  response.send('Hello World');
 });
 
 app.get('/new', (request, response) => {
@@ -69,13 +67,13 @@ app.get('/new', (request, response) => {
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
 let onClose = function(){
-  
+
   console.log("closing");
-  
+
   server.close(() => {
-    
+
     console.log('Process terminated');
-    
+
     pool.end( () => console.log('Shut down db connection pool'));
   })
 };
