44fa00970c329e38ed7703d1262d6069dd9af493
sean chan <sean@seans-MacBook-Air.local>
2019-01-09 17:01:15 +0800

    Index done

---
+++ views/defaultcss.jsx
@@ -0,0 +1,54 @@
+var React = require('react');
+import React, { Component } from 'react';
+
+class DefaultLayout extends React.Component {
+  render() {
+    return (
+      <html>
+        <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossOrigin="anonymous" />
+        <link rel="stylesheet" type="text/css" href="/css/style.css" />
+        <link href="https://fonts.googleapis.com/css?family=ZCOOL+KuaiLe" rel="stylesheet" />
+        <title>Tunr</title>
+        </head>
+        <header>
+
+        </header>
+        <body className="container-fluid">
+        <nav>
+            <ul className="nav justify-content-center">
+              <li className="nav-item">
+                <a className="nav-link text-info" href="/recipe">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-info" href="/recipe/new">Create song</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-info" href="/recipe/ingredients">Artists</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contact</a>
+              </li>
+            </ul>
+        </nav>
+            <div className="container">
+              <div className="row">
+                <div className="col-12">
+                  {this.props.children}
+                </div>
+              </div>
+            </div>
+        <footer className="sticky-bottom">
+            Copyright 2019 <span> </span>
+            Created with &hearts; by <span>Sean Chan</span><span> </span>
+            Github: @https://github.com/seany94
+        </footer>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossOrigin="anonymous"></script>
+        </body>
+      </html>
+    );
+  }
+}
+
+module.exports = DefaultLayout;
--- views/home.jsx
+++ views/home.jsx
@@ -1,14 +1,57 @@
 var React = require("react");
+var Defaultcss = require('./defaultcss');
+
+class Artistlist extends React.Component{
+    render(){
+        return(
+            <div>
+                <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title text-danger" id="exampleModalLabel">WARNING!</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div className="modal-body text-danger">
+                    You are about to delete a recipe from the database. Click <strong>close</strong> to return to main menu or click <strong>confirm</strong> to proceed with the deletion.
+                  </div>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <form method="POST" action={"/recipe/" + this.props.list.id + "?_method=delete"}>
+                        <button type="submit" value="Delete" className="btn btn-primary">Confirm</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+                <div className="homecont">
+                    <ul>
+                        Artist: {this.props.list.name} <br />
+                        <form method="GET" action={"/recipe/" + this.props.list.id + "/details"}>
+                            <input type="submit" className="details" value="Details" />
+                        </form>
+                        <form method="GET" action={"/recipe/" + this.props.list.id + "/edit"}>
+                            <input type="submit" className="edit" value="Edit artist" />
+                        </form>
+                        <input type="submit" className="delete" value="Delete" data-toggle="modal" data-target="#exampleModal"/>
+                    </ul>
+                </div>
+            </div>
+            );
+    }
+}
 
 class Home extends React.Component {
   render() {
+    const artists = this.props.list.map( artist => {
+            return <Artistlist list={artist}></Artistlist>;
+        });
     return (
-      <html>
-        <head />
-        <body>
-          <h1>Welcome!</h1>
-        </body>
-      </html>
+        <Defaultcss>
+            {artists}
+        </Defaultcss>
     );
   }
 }
