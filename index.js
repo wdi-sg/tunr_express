91a9017877e856e7e09094177cf620cc83a417bc
angrylobster <liewminshan@gmail.com>
2019-01-09 14:53:02 +0800

    Part 1 Completed

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ const pg = require('pg');
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
-  host: '127.0.0.1',
-  database: 'tunr_db',
-  port: 5432,
+    user: 'postgres',
+    host: '127.0.0.1',
+    database: 'tunr_db',
+    port: 5432,
+    password: 'postgres'
 };
 
 const pool = new pg.Pool(configs);
 
 pool.on('error', function (err) {
-  console.log('idle client error', err.message, err.stack);
+    console.log('idle client error', err.message, err.stack);
 });
 
 /**
@@ -30,7 +31,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }));
 
 app.use(methodOverride('_method'));
@@ -48,16 +49,17 @@ app.engine('jsx', reactEngine);
  * ===================================
  */
 
-app.get('/', (req, res) => {
-  // query database for all pokemon
+app.get('/', (request, response) => {
+    // query database for all pokemon
 
-  // respond with HTML page displaying all pokemon
-  response.render('home');
+    // respond with HTML page displaying all pokemon
+    response.send('Hello World');
+    // response.render('home');
 });
 
 app.get('/new', (request, response) => {
-  // respond with HTML page with form to create new pokemon
-  response.render('new');
+    // respond with HTML page with form to create new pokemon
+    response.render('new');
 });
 
 
@@ -68,16 +70,16 @@ app.get('/new', (request, response) => {
  */
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
-let onClose = function(){
-  
-  console.log("closing");
-  
-  server.close(() => {
-    
-    console.log('Process terminated');
-    
-    pool.end( () => console.log('Shut down db connection pool'));
-  })
+let onClose = function () {
+
+    console.log("closing");
+
+    server.close(() => {
+
+        console.log('Process terminated');
+
+        pool.end(() => console.log('Shut down db connection pool'));
+    })
 };
 
 process.on('SIGTERM', onClose);
