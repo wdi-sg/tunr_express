a13869d4e51d6eabc3abcf837c1660574a1de86f
SiewLeng <siewleng.lim@gmail.com>
2019-01-10 16:34:07 +0800

    tunr

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const pg = require('pg');
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
+  user: 'apple',
   host: '127.0.0.1',
   database: 'tunr_db',
   port: 5432,
@@ -51,15 +51,44 @@ app.engine('jsx', reactEngine);
 app.get('/', (req, res) => {
   // query database for all pokemon
 
-  // respond with HTML page displaying all pokemon
-  response.render('home');
+  // respond with HTML page displaying all artist
+    let queryText = "SELECT * FROM artists"
+    pool.query(queryText, (err, queryResult) => {
+        res.send(queryResult.rows);
+    })
 });
 
-app.get('/new', (request, response) => {
+
+app.get ('/:id', (req, res) => {
+    let id = req.params.id;
+    let queryText1 =`SELECT * FROM artists WHERE id=${id}`;
+    pool.query(queryText1, (err, queryResult) => {
+        res.send(queryResult.rows[0]);
+    })
+})
+
+
+app.get ('/edit/:id', (req, res) => {
+    let id = req.params.id;
+    let queryText2 =`SELECT * FROM artists WHERE id=${id}`;
+    pool.query(queryText2, (err, queryResult) => {
+        res.render('edit', queryResult.rows[0]);
+    })
+})
+
+app.get('/new/artist', (request, response) => {
   // respond with HTML page with form to create new pokemon
-  response.render('new');
+  response.render('new',{});
 });
 
+
+app.post('/new/artist/updated', (request, response)=> {
+    response.send(request.body);
+    let queryText = 'INSERT INTO artists (name, photo_url, nationality) VALUES ($1, $2, $3) RETURNING id';
+    const values = [request.body.name, request.body.photo_url, request.body.nationality]
+    pool.query(queryText, values, (err, res) => {
+    });
+});
 
 /**
  * ===================================
@@ -69,13 +98,13 @@ app.get('/new', (request, response) => {
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
 let onClose = function(){
-  
+
   console.log("closing");
-  
+
   server.close(() => {
-    
+
     console.log('Process terminated');
-    
+
     pool.end( () => console.log('Shut down db connection pool'));
   })
 };
--- views/home.jsx
+++ views/home.jsx
@@ -4,7 +4,7 @@ class Home extends React.Component {
   render() {
     return (
       <html>
-        <head />
+        <head/>
         <body>
           <h1>Welcome!</h1>
         </body>
