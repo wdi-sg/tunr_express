f926a3a24b3af58465d0e9524fdb7258a46a4444
reynosoturtle <christianreynoso96@gmail.com>
2019-04-09 21:03:32 +0800

    ready to start building app

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const pg = require('pg');
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
+  user: 'Chris',
   host: '127.0.0.1',
   database: 'tunr_db',
   port: 5432,
@@ -52,7 +52,7 @@ app.get('/', (request, response) => {
   // query database for all pokemon
 
   // respond with HTML page displaying all pokemon
-  response.render('home');
+  response.send('hello');
 });
 
 app.get('/new', (request, response) => {
@@ -69,13 +69,13 @@ app.get('/new', (request, response) => {
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
 let onClose = function(){
-  
+
   console.log("closing");
-  
+
   server.close(() => {
-    
+
     console.log('Process terminated');
-    
+
     pool.end( () => console.log('Shut down db connection pool'));
   })
 };
