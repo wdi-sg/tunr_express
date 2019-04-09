4636bdbb81064b312a819d6a94f993c51e0c0dd2
seeo <aragonlotr5@gmail.com>
2019-04-10 05:52:01 +0800

    managed to do a songs.jsx view page but I think all the album art work links are corrupted. Also I am wondering how to use sound-react to build in sound playing capability...

--- views/songs.jsx
+++ views/songs.jsx
@@ -2,6 +2,27 @@ var React = require('react');
 var Layout = require("./layout");
 var Sound = require('react-sound');
 
+// class Sound extends React.Component{
+//   render(){
+//     let allArtistPlaySongsArray = this.props.artistId.map(songItem=>{
+//        let preview_sound = (songItem.preview_link).replace(/\'/g, "");
+//     })
+//     return (
+//         <div>
+//           <Sound
+//             url ="preview_sound"
+//             playStatus={Sound.status.PLAYING}
+//             playFromPosition={300 /* in milliseconds */}
+//             onLoading={this.handleSongLoading}
+//             onPlaying={this.handleSongPlaying}
+//             onFinishedPlaying={this.handleSongFinishedPlaying}
+//           />
+//         </div>
+//     )
+//   }
+// }
+// {allArtistPlaySongsArray}
+
 class Songs extends React.Component {
 
   render() {
@@ -9,8 +30,8 @@ class Songs extends React.Component {
       let id = parseInt(songItem.id);
       let title = songItem.title;
       let album = songItem.album; //this is a string, how to remove the quotes?
-      let preview_link = (songItem.preview_link).replace(/\'/g, "") ;
-      let artwork = (songItem.artwork).replace(/\'/g, "") ;
+      let preview_link = (songItem.preview_link).replace(/\'/g, "");
+      let artwork = (songItem.artwork).replace(/\'/g, "");
       let artist_id = parseInt(songItem.artist_id);
         return(
           // <li>{id} {num} {name}</li>
@@ -32,6 +53,7 @@ class Songs extends React.Component {
       <Layout>
         <div>
           {allArtistSongsArr}
+
         </div>
       </Layout>
     );
