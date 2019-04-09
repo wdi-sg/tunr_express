c8cdfe04835fecaeea1714e8147afb44eb424108
claucanchin <claucanchin@gmail.com>
2019-04-09 19:22:31 +0800

    completed part 1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const pg = require('pg');
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
+  user: 'claucanchin',
   host: '127.0.0.1',
   database: 'tunr_db',
   port: 5432,
@@ -49,14 +49,14 @@ app.engine('jsx', reactEngine);
  */
 
 app.get('/', (request, response) => {
-  // query database for all pokemon
+  // query database for all songs
 
-  // respond with HTML page displaying all pokemon
+  // respond with HTML page displaying all songs
   response.render('home');
 });
 
 app.get('/new', (request, response) => {
-  // respond with HTML page with form to create new pokemon
+  // respond with HTML page with form to create new songs
   response.render('new');
 });
 
@@ -69,13 +69,13 @@ app.get('/new', (request, response) => {
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
 let onClose = function(){
-  
+
   console.log("closing");
-  
+
   server.close(() => {
-    
+
     console.log('Process terminated');
-    
+
     pool.end( () => console.log('Shut down db connection pool'));
   })
 };
