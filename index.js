d26b3d523c99c7fe58114ac3424426c6266fb296
Muhdhatta <muhdhatta.alchemist@gmail.com>
2019-04-10 09:23:32 +0800

    hatta

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const pg = require('pg');
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
+  user: 'asadullah',
   host: '127.0.0.1',
   database: 'tunr_db',
   port: 5432,
@@ -61,6 +61,61 @@ app.get('/new', (request, response) => {
 });
 
 
+
+
+app.get('/artist_data/:id', (request, response) => {
+
+  // SELECT * FROM STUDENTS WHERE id =
+
+  const queryString = 'SELECT * from songs WHERE id='+request.params.id;
+  //     response.send( queryString );
+  // return;
+
+  pool.query(queryString, (errorObj, result) => {
+
+    console.log( errorObj, result );
+
+    // errorObj is not null if there's an error
+    if (errorObj === undefined ) {
+
+      console.log('query resulttttttt:', result.rows);
+      // const data = {  students : result.rows};
+      response.send( result.rows );
+    } else {
+
+      console.error('query error:', errorObj.stack);
+      response.send( 'query error' );
+
+    }
+  });
+
+
+});
+
+
+
+
+
+app.get('/home', (request, response) => {
+
+  const queryString = 'SELECT * from artist_data';
+
+  pool.query(queryString, (errorObj, result) => {
+
+    // errorObj is not null if there's an error
+    if (errorObj === null) {
+
+      console.log('query resulttttttt:', result.rows);
+      const data = {  artist_data : result.rows};
+      response.render('home', data);
+    } else {
+
+      console.error('query error:', errorObj.stack);
+      response.send( 'query error' );
+
+    }
+  });
+});
 /**
  * ===================================
  * Listen to requests on port 3000
@@ -69,13 +124,13 @@ app.get('/new', (request, response) => {
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
 let onClose = function(){
-  
+
   console.log("closing");
-  
+
   server.close(() => {
-    
+
     console.log('Process terminated');
-    
+
     pool.end( () => console.log('Shut down db connection pool'));
   })
 };
