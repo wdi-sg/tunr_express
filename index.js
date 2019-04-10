ba3177e3d9c1b6c5133961d9b104cd11d28e555f
calyeap <calvinyeap.h@gmail.com>
2019-04-10 21:45:33 +0800

    part 1 deliverables

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 console.log("starting up!!");
 
-const express = require('express');
-const methodOverride = require('method-override');
-const pg = require('pg');
+const express = require("express");
+const methodOverride = require("method-override");
+const pg = require("pg");
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
-  host: '127.0.0.1',
-  database: 'tunr_db',
-  port: 5432,
+  user: "cyeap",
+  host: "127.0.0.1",
+  database: "tuner_db",
+  port: 5432
 };
 
 const pool = new pg.Pool(configs);
 
-pool.on('error', function (err) {
-  console.log('idle client error', err.message, err.stack);
+pool.on("error", function(err) {
+  console.log("idle client error", err.message, err.stack);
 });
 
-/**
+/**t
  * ===================================
  * Configurations and set up
  * ===================================
@@ -26,21 +26,20 @@ pool.on('error', function (err) {
 
 // Init express app
 const app = express();
-
-
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
 
-app.use(methodOverride('_method'));
-
+app.use(methodOverride("_method"));
 
 // Set react-views to be the default view engine
-const reactEngine = require('express-react-views').createEngine();
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jsx');
-app.engine('jsx', reactEngine);
+const reactEngine = require("express-react-views").createEngine();
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", reactEngine);
 
 /**
  * ===================================
@@ -48,37 +47,45 @@ app.engine('jsx', reactEngine);
  * ===================================
  */
 
-app.get('/', (request, response) => {
-  // query database for all pokemon
-
-  // respond with HTML page displaying all pokemon
-  response.render('home');
+app.get("/", (request, response) => {
+  // form query string
+  let queryText = "SELECT * FROM artists";
+  // query database for so and so
+  pool.query(queryText, (err, result) => {
+    if (err) {
+      console.log("query error", err.message);
+      response.send("query error");
+    } else {
+      // respond with HTML page displaying welcome message
+      // console.log(result.rows);
+      response.render("home", { artists: result.rows });
+    }
+  });
 });
 
-app.get('/new', (request, response) => {
-  // respond with HTML page with form to create new pokemon
-  response.render('new');
+app.get("/new", (request, response) => {
+  // respond with HTML page with new form - to update
+  response.render("new");
 });
-
 
 /**
  * ===================================
  * Listen to requests on port 3000
  * ===================================
  */
-const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+const server = app.listen(3000, () =>
+  console.log("~~~ Tuning in to the waves of port 3000 ~~~")
+);
 
-let onClose = function(){
-  
+let onClose = function() {
   console.log("closing");
-  
+
   server.close(() => {
-    
-    console.log('Process terminated');
-    
-    pool.end( () => console.log('Shut down db connection pool'));
-  })
+    console.log("Process terminated");
+
+    pool.end(() => console.log("Shut down db connection pool"));
+  });
 };
 
-process.on('SIGTERM', onClose);
-process.on('SIGINT', onClose);
+process.on("SIGTERM", onClose);
+process.on("SIGINT", onClose);
