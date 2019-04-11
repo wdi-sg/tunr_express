7c4c9b1c8fabe95af7bfc5a131054b51e05cd915
ricenzca <ricenzca@hotmail.com>
2019-04-11 09:17:07 +0800

    done songs for artist page, add song to playlist in progress

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ pool.on('error', function (err) {
 // Init express app
 const app = express();
 
-
+app.use(express.static('Public'));
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
@@ -44,14 +44,19 @@ app.set('view engine', 'jsx');
 app.engine('jsx', reactEngine);
 
 /**
+ *
+ *
  * ===================================
- * Routes
+ * Routes Part 1
  * ===================================
+ *
+ *
+ *
  */
 
 app.get('/artists', (request, response) => {
 
-  const queryString = "SELECT * FROM artists";
+  const queryString = "SELECT * FROM artists ORDER BY id";
 
   pool.query(queryString, (err, result) => {
 
@@ -126,7 +131,7 @@ app.get('/artists/:id/delete', (request, response) => {
 
   const id = request.params.id;
 
-  const queryString = `SELECT * FROM artists WHERE id='${id}'`;
+  const queryString = `SELECT * FROM artists WHERE id='${id}' ORDER BY id`;
 
   pool.query(queryString, (err, result) => {
 
@@ -177,13 +182,134 @@ app.get('/artists/:id', (request, response) => {
 });
 
 
+/**
+ *
+ *
+ * ===================================
+ * Part 2: Routes for songs of a particular artist
+ * ===================================
+ *
+ *
+ *
+ */
+app.get('/artists/:id/songs', (request, response) => {
+
+  const artistId = request.params.id;
+  const queryString = `SELECT *, songs.id FROM songs INNER JOIN artists ON (songs.artist_id=artists.id) where artist_id=${artistId}`;
+
+  pool.query(queryString, (err, result) => {
+
+    if (err) {
+      console.error('203 query error:', err.stack);
+      response.send( 'query error' );
+    } else {
+      const data = { songs: result.rows };
+      response.render('listSongsOfAnArtist', data );
+    }
+  });
+});
+
+app.get('/artists/:id/songs/new', (request, response) => {
+
+  const id = request.params.id;
+  const queryString = `SELECT * FROM artists WHERE id=${id}`;
+
+  pool.query(queryString, (err, result) => {
+
+    if (err) {
+      console.error('220 query error:', err.stack);
+      response.send( 'query error' );
+    } else {
+      const data = { artist: result.rows };
+      response.render('newSong', data );
+    }
+  });
+});
+
+app.post('/artists/:id/songs', (request, response) => {
+
+  const id = request.params.id;
+  const queryString = `INSERT INTO songs (title, album, preview_link, artwork, artist_id) VALUES ($1,$2,$3,$4,$5)`;
+  let values = Object.values(request.body);
+  values.push(id);
+  pool.query(queryString, values, (err, result) => {
+
+    if (err) {
+      console.error('241 query error:', err.stack);
+      response.send( 'query error' );
+    } else {
+      response.redirect(`/artists/${id}/songs`);
+    }
+  });
+});
+
+
+/**
+ *
+ *
+ * ===================================
+ * Part 2: Routes for playlist
+ * ===================================
+ *
+ *
+ *
+ */
+
+// app.get('/playlist', (request, response) => {
+//   const queryString = "SELECT * FROM playlist ORDER BY id";
+
+//   pool.query(queryString, (err, result) => {
+
+//     if (err) {
+//       console.error('264 query error:', err.stack);
+//       response.send( 'query error' );
+//     } else {
+//       const data = { playlist: result.rows };
+//       response.render('playlistHome', data );
+//     }
+//   });
+// });
+
+// app.get('/playlist/new', (request, response) => {
+//   response.render('newPlaylist');
+// });
+
+// app.post('/playlist', (request, response) => {
+  
+//   const array = Object.values(request.body);
+//   const playlist_name = array.shift();
+//   const values = [];
+//   const valueString = "";
+//   for (i=0; i<array.length; i++) {
+//   	values.push(playlist_name);
+//   	values.push(array[i]);
+//   	valuesString += `$${i+1}`;
+//   }
+//   console.log("288");
+//   console.log(values);
+//   console.log(valuesString);
+
+  // const queryString = `INSERT INTO playlist_songs(playlist_name, song_title) VALUES($1,$2)`;
+  
+
+
+  // pool.query(queryString, values, (err, result) => {
+  //   if (err) {
+  //     console.error('270 query error:', err.stack);
+  //     response.send( 'query error' );
+  //   } else {
+  //     response.redirect(`/playlist`);
+  //   }
+  // });
+// });
+
 
 /**
  * ===================================
  * Listen to requests on port 3000
  * ===================================
  */
-const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+const server = app.listen(8080, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
 let onClose = function(){
   
---
+++ views/newSong.jsx
@@ -0,0 +1,27 @@
+var React = require("react");
+
+class NewSong extends React.Component {
+  render() {
+    const artistName = this.props.artist[0].name;
+    const id = this.props.artist[0].id;
+    const postPath = `/artists/${id}/songs`;
+    return (
+      <html>
+        <body>
+        <h1>Add New Song for {artistName}</h1>
+          <h3>
+          <form method="post" action={postPath} >
+            <p>Song Title: </p><input type="text" name="title" />
+            <p>Album: </p><input type="text" name="album" />
+            <p>Preview Link: </p><input type="text" name="previewLink" />
+            <p>Artwork: </p><input type="text" name="artwork" />
+            <br/><input type="submit" value="Submit" />
+          </form>
+          </h3>
+        </body>
+      </html>
+    );
+  }
+}
+
+module.exports = NewSong;
