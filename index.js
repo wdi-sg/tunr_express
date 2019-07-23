d7d0fa79da76a8b792ccca4bed0c66ce88b5fe4b
MalcolmLow83 <malcomlow@gmail.com>
2019-07-23 19:46:42 +0800

    Completion of index feature

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const pg = require('pg');
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
+  user: 'malcolmlow',
   host: '127.0.0.1',
   database: 'tunr_db',
   port: 5432,
@@ -48,12 +48,27 @@ app.engine('jsx', reactEngine);
  * ===================================
  */
 
+// The index feature (Deliverable)
 app.get('/', (request, response) => {
-  // query database for all pokemon
 
-  // respond with HTML page displaying all pokemon
-  response.render('home');
+  const queryString = 'SELECT * from artists'
+
+  pool.query(queryString, (err, result) => {
+
+    if (err) {
+        console.log('query error:', err.stack );
+        response.send('query error:');
+    }
+    else {
+
+        // console.log('query result:', result.rows);
+        response.render('home', result);
+    }
+  })
 });
+
+// ===================================
+
 
 app.get('/new', (request, response) => {
   // respond with HTML page with form to create new pokemon
@@ -69,13 +84,13 @@ app.get('/new', (request, response) => {
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
 let onClose = function(){
-  
+
   console.log("closing");
-  
+
   server.close(() => {
-    
+
     console.log('Process terminated');
-    
+
     pool.end( () => console.log('Shut down db connection pool'));
   })
 };
--- views/home.jsx
+++ views/home.jsx
@@ -2,11 +2,20 @@ var React = require("react");
 
 class Home extends React.Component {
   render() {
+
+    const artistsList = this.props.rows.map(artistName =>{
+
+        return <li>{artistName.name}</li>
+    })
+
     return (
       <html>
         <head />
         <body>
-          <h1>Welcome!</h1>
+          <h1>All Artists Names</h1>
+          <ul>
+            {artistsList}
+          </ul>
         </body>
       </html>
     );
