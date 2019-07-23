88ed9ad755e99fbcdc4e4fb220e5ef5e281220dc
gitwenlei <ngwenlei@gmail.com>
2019-07-24 03:21:52 +0800

    Clean up html tags

--- views/artist.jsx
+++ views/artist.jsx
@@ -7,11 +7,9 @@ class Artist extends React.Component {
         <head />
         <body>
         <h1>Artist</h1>
-        <div key={this.props.artists.name} className="artist">
             <img className="artist-photo" src={this.props.artists.photo_url} />
             <p className="artist-name">{this.props.artists.name}</p>
             <p className="artist-nat">{this.props.artists.nationality}</p>
-        </div>
         </body>
       </html>
     );
