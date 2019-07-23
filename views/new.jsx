39e562f0f12d9b105e1a18b3abfef43692d9f8fa
gitwenlei <ngwenlei@gmail.com>
2019-07-24 01:16:48 +0800

    Created form page for adding new artist

--- views/new.jsx
+++ views/new.jsx
@@ -7,6 +7,21 @@ class New extends React.Component {
         <head />
         <body>
           <h3>Form Goes Here!</h3>
+          <h1>Add New Artist to Our Current Artists!</h1>t
+              <form method="POST" action="/artist">
+                <p>Artist Name: </p>
+                <input className="artist-name" type="text" name="name" placeholder="enter artist name" />
+
+                 <p>Artist Photo: </p>
+                <input className="artist-pho
+                to" type="text" name="photo_url" placeholder="enter artist photo url" />
+
+                <p>Artist Nationality: </p>
+                <input className="artist-nationality" type="text" name="nationality" placeholder="enter artist nationality" />
+
+                <input className="submit-btn" type="submit" value="Add" />
+              </form>
+
         </body>
       </html>
     );
