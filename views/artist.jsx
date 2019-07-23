fbe733b6272f2e4c0b0ae57bd9bb43f72a8a002b
gitwenlei <ngwenlei@gmail.com>
2019-07-24 01:15:56 +0800

    Created display artist page

--- views/artist.jsx
+++ views/artist.jsx
@@ -2,25 +2,15 @@ var React = require("react");
 
 class Artist extends React.Component {
   render() {
-    const artistList = this.props.artists.map(artist =>
-        <div key={artist.name} className="artist">
-            <img className="artist-photo" src={artist.photo_url} />
-            <p className="artist-id">{artist.id}</p>
-            <p className="artist-name">{artist.name}</p>
-            <p className="artist-nat">{artist.nationality}</p>
-        </div>
-        );
     return (
       <html>
         <head />
         <body>
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                  <h1>Welcome!</h1>
-                  {artistList}
-                </div>
-            </div>
+        <h1>Artist</h1>
+        <div key={this.props.artists.name} className="artist">
+            <img className="artist-photo" src={this.props.artists.photo_url} />
+            <p className="artist-name">{this.props.artists.name}</p>
+            <p className="artist-nat">{this.props.artists.nationality}</p>
         </div>
         </body>
       </html>
