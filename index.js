f06e4aaa76bf388a1d987967ba9e0fb526253dac
gitwenlei <ngwenlei@gmail.com>
2019-07-23 22:51:04 +0800

    Set up express app

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const pg = require('pg');
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
+  user: 'fishie',
   host: '127.0.0.1',
   database: 'tunr_db',
   port: 5432,
@@ -49,11 +49,21 @@ app.engine('jsx', reactEngine);
  */
 
 app.get('/', (request, response) => {
-  // query database for all pokemon
-
-  // respond with HTML page displaying all pokemon
-  response.render('home');
+    const queryString = `SELECT * FROM Artists`;
+    pool.query(queryString, (err, result) => {
+        if (err) {
+            console.error('query error:', err.stack);
+            response.send( 'query error' );
+        } else {
+            console.log('query result:', result);
+            // redirect to home page
+            // response.send( result.rows );
+            response.send('hello world');
+        }
+    });
 });
+
+
 
 app.get('/new', (request, response) => {
   // respond with HTML page with form to create new pokemon
@@ -69,15 +79,11 @@ app.get('/new', (request, response) => {
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
 let onClose = function(){
-  
-  console.log("closing");
-  
-  server.close(() => {
-    
-    console.log('Process terminated');
-    
-    pool.end( () => console.log('Shut down db connection pool'));
-  })
+    console.log("closing");
+    server.close(() => {
+        console.log('Process terminated');
+        pool.end( () => console.log('Shut down db connection pool'));
+    })
 };
 
 process.on('SIGTERM', onClose);
