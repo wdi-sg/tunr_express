c7aa4a03467a2a0cb0a0955798181a3086a2edd2
emijay <hilmi.johari@yahoo.com.sg>
2019-07-25 18:13:23 +0800

    minor code changes for esthetics

--- views/register.jsx
+++ views/register.jsx
@@ -5,7 +5,7 @@ class Register extends React.Component {
     return (
       <html>
         <head>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous"></link>
             <link href="https://fonts.googleapis.com/css?family=Lora|Roboto&display=swap" rel="stylesheet"></link>
         </head>
         <body style={{backgroundImage : 'url("https://wallpaperaccess.com/full/646093.png")', backgroundPosition : "center", backgroundRepeat : "no-repeat", backgroundSize : "cover", height : "100vh", backgroundAttachment : "fixed"}}>
@@ -14,16 +14,15 @@ class Register extends React.Component {
         <h1 className="text-center my-5 text-white" style={{fontFamily : "Lora, serif"}}>Hello First Time User! <br/> Please Register Below</h1>
 
             <form action="/users" method="POST" className = "text-white">
-              <div class="form-group">
-                <label for="inputUsername">Username</label>
-                <input type="username" name="username" class="form-control" id="inputUsername" aria-describedby="emailHelp" placeholder="Enter username" />
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+              <div className="form-group">
+                <label htmlFor="inputUsername">Username</label>
+                <input type="username" name="username" className="form-control" id="inputUsername" aria-describedby="emailHelp" placeholder="Enter username" />
               </div>
-              <div class="form-group">
-                <label for="inputPassword">Password</label>
-                <input type="password" name="password" class="form-control" id="inputPassword" placeholder="Password" />
+              <div className="form-group">
+                <label htmlFor="inputPassword">Password</label>
+                <input type="password" name="password" className="form-control" id="inputPassword" placeholder="Password" />
               </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
+              <button type="submit" className="btn btn-primary">Submit</button>
             </form>
           </div>
         </body>
