7735d652423635f167d4be77742979b58c3a9295
EliseChia <swedesurrender@gmail.com>
2019-07-25 21:14:14 +0800

    added styling for redirection page

--- views/loggingin.jsx
+++ views/loggingin.jsx
@@ -4,9 +4,14 @@ class Loggingin extends React.Component {
   render() {
     return (
       <html>
-        <head />
-        <body>
-            <h1>Success</h1>
+        <head>
+            <link href="https://fonts.googleapis.com/css?family=Darker+Grotesque:400,600&display=swap" rel="stylesheet"/>
+            <link rel="stylesheet" type="text/css" href="/style.css"/>
+        </head>
+        <body className="logging-in">
+            <img id="logo" src="logo.png"/>
+            <br></br>
+            <h1>Success!</h1>
             <p>Please hold on while you are redirected.</p>
             <script type="text/javascript" src="/loggingin.js"></script>
         </body>
