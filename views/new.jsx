e83df605ec04435a7b2f27968037374f5c25b6f9
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-10-15 21:46:36 +0800

    created and styled add new arttist form

--- views/new.jsx
+++ views/new.jsx
@@ -4,10 +4,24 @@ class New extends React.Component {
   render() {
     return (
       <html>
-        <head />
+        <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville&display=swap" rel="stylesheet"></link>
+        </head>
         <body>
-          <h3>Form Goes Here!</h3>
-        </body>
+         <main class="form-row" style={{ margin: '50px auto',  width: '500px', fontFamily: 'Montserrat'}} >
+        <h3 class="text-center" style={{width: '500px'}}>Add New Artist: </h3><br/>
+
+          <form method="post" action="/artists" style={{width: '500px'}} ><br/><br/>
+            Artist Name: <input type="text" name="name" style={{width: '500px'}} /><br/><br/>
+            Nationality: <input type="text" name="nationality" style={{width: '500px'}} /><br/><br/>
+            Image URL: <input type="text" name="url"  style={{width: '500px'}}/><br/><br/>
+            <input type="submit" value="Submit" class="btn btn-primary btn-block" style={{width: '500px'}} />
+          </form>
+
+          </main>
+          </body>
       </html>
     );
   }
