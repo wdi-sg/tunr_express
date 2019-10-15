d87c0413e0c477a6d0f84a00eded33cd64952e23
lhjd <hongjinlau@gmail.com>
2019-10-15 20:15:14 +0800

    Create tables

---
+++ tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE songs (
+    id SERIAL PRIMARY KEY,
+   title TEXT, 
+   album TEXT, 
+   preview_link TEXT, 
+   artwork TEXT, 
+   artist_id INTEGER
+);
+
+CREATE TABLE artists (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    photo_url TEXT,
+    nationality TEXT
+);
