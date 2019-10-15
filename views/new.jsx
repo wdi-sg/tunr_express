bb86118da1e1bcdb42bef7076c5bd0effc662d5c
David M <davidmosk@gmail.com>
2019-10-15 18:17:29 +0800

    edit page and new page updated to artists

--- views/new.jsx
+++ views/new.jsx
@@ -1,12 +1,26 @@
-var React = require("react");
-
+var React = require('react');
 class New extends React.Component {
   render() {
     return (
       <html>
-        <head />
         <body>
-          <h3>Form Goes Here!</h3>
+                  <div>
+                  { this.props.warning }
+
+            <h1>Add a New Artist</h1>
+            <form method="POST" action="/artists">
+            <br/>
+            Name: <input type="text" name="name"/>
+            <br/>
+            PhotoURL: <input type="text" name="photo_url"/>
+            <br/>
+            Nationality: <input type="text" name="nationality"/>
+            <br/>
+            <input type="submit" value="Submit"/>
+            </form>
+
+
+          </div>
         </body>
       </html>
     );
