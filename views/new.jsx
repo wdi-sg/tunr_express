2e544d51f7b6f661d261b7a86d722579514db060
dannyozh <danielongzhihao@gmail.com>
2019-10-15 17:15:02 +0800

    made minor edits to home.jsx

--- views/new.jsx
+++ views/new.jsx
@@ -1,16 +1,30 @@
-var React = require("react");
+var React = require('react');
 
 class New extends React.Component {
-  render() {
-    return (
-      <html>
-        <head />
-        <body>
-          <h3>Form Goes Here!</h3>
-        </body>
-      </html>
-    );
-  }
+	render() {
+		return (
+			<html>
+				<head />
+				<body>
+					<h3>Add a new artist here!</h3>
+					<br />
+					<form method="POST" action="/artists">
+						<p>Artist Name:</p>
+						<input type="text" name="name" />
+						<br />
+						<p>Artist URL:</p>
+						<input type="url" name="photo_url" />
+			      <br />
+						<p>Artist Nationality:</p>
+						<input type="text" name="nationality" />
+						<br />
+						<input type="submit" value="Submit" />
+						<br />
+					</form>
+				</body>
+			</html>
+		);
+	}
 }
 
 module.exports = New;
