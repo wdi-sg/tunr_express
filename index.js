438c9963810fb39d7bfe407dca1a030aae894520
David M <davidmosk@gmail.com>
2019-10-15 18:16:52 +0800

    add new artist added

--- index.js
+++ index.js
@@ -57,10 +57,57 @@ app.get('/', (request, response) => {
   response.render('home');
 });
 
-app.get('/new', (request, response) => {
+app.get('/artists/new', (request, response) => {
   // respond with HTML page with form to create new pokemon
   response.render('new');
 });
+
+
+// add to the database a new artist
+app.post('/artists', (request, response) => {
+let data = {warning: ""};
+console.log('post to artists');
+  if (request.body.name === "" || request.body.photo_url === "" || request.body.nationality === "") {
+    data = {warning: "Empty name or other data..."}; 
+      } else  { 
+    data = {warning: "Artist Added!"}; 
+      }
+
+let queryText = 'INSERT INTO artists (name, photo_url, nationality) VALUES ($1, $2, $3) RETURNING id';
+
+const values = [request.body.name , request.body.photo_url, request.body.nationality];
+
+ pool.query(queryText, values, (err, res) => {
+       if (err) {
+         console.log("query error", err.message);
+       } else {
+         console.log("id of the thing you just created:", res.rows[0].id);
+       }
+   });
+
+  response.render('new', data);
+
+
+});
+
+
+app.get('/artists/:id/edit', (request, response) => {
+    let inputId = parseInt( request.params.id )
+  const queryString = 'SELECT * from artists WHERE id='+inputId;
+
+  pool.query(queryString, (err, result) => {
+    // obj is the object from the pokedex json file
+    // extract input data from request
+    console.log(err);
+    // let inputId = parseInt( request.params.id );
+    const data = result.rows[0];
+
+
+  response.render('edit', data);
+    
+      });
+});
+
 
 
 /**
