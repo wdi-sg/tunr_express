2d72295fe93e50a11bc95e135b5610303ec54d8f
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-10-16 09:37:45 +0800

    updated the home render

--- views/home.jsx
+++ views/home.jsx
@@ -2,15 +2,35 @@ var React = require("react");
 
 class Home extends React.Component {
   render() {
-    return (
+    const artistsList = this.props.artists.map(item => {
+
+        return (
+        <div>
+        <h1>Welcome!</h1>
+          <h5>Id: {item.id}</h5>
+          <h5>Artist Name: {item.name} </h5>
+          <h5>Artist Nationality: {item.nationality}</h5>
+          <img width="500px" src={item.photo_url} />
+          </div>
+          )
+});
+
+
+         return (
       <html>
         <head />
         <body>
-          <h1>Welcome!</h1>
+          {artistsList}
         </body>
       </html>
     );
-  }
+
+
+
+
+}
+
+
 }
 
 module.exports = Home;
