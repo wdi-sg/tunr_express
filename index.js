44535c0ea41e5e8d646dd1adfe17d5b33e4a4cae
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-10-16 09:44:13 +0800

    updater with view all and show single artist

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const pg = require('pg');
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
-  host: '127.0.0.1',
-  database: 'tunr_db',
-  port: 5432,
+    user: 'mariadimitrijevic',
+    host: '127.0.0.1',
+    database: 'tunr_db',
+    port: 5432,
 };
 
 const pool = new pg.Pool(configs);
 
-pool.on('error', function (err) {
-  console.log('idle client error', err.message, err.stack);
+pool.on('error', function(err) {
+    console.log('idle client error', err.message, err.stack);
 });
 
 /**
@@ -30,7 +30,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }));
 
 app.use(methodOverride('_method'));
@@ -48,17 +48,69 @@ app.engine('jsx', reactEngine);
  * ===================================
  */
 
+//index - render home
 app.get('/', (request, response) => {
-  // query database for all pokemon
+    // query database for all artists
 
-  // respond with HTML page displaying all pokemon
-  response.render('home');
+    // respond with HTML page displaying all artists
+    response.render('home');
 });
 
-app.get('/new', (request, response) => {
-  // respond with HTML page with form to create new pokemon
-  response.render('new');
+//table for creating new artist
+app.get('/artists/new', (request, response) => {
+    // respond with HTML page with form to create new artist
+    response.render('new');
 });
+
+
+app.get('/artists', (request, response) => {
+
+    // const data = {artists: result.rows};
+
+
+    const queryText = "SELECT * FROM artists";
+
+    pool.query(queryText, (err, queryRes) => {
+        // const data = {artists: queryRes.rows};
+
+         const artistsDB = queryRes.rows;
+         const data = {
+            artists: artistsDB
+        };
+
+        // if (err) {
+        //     return console.error('Error executing query', err.stack)
+        // }
+
+
+        console.log(queryRes.rows);
+        response.render('home', data);
+    });
+});
+
+
+
+
+
+//show single artist
+app.get('/artists/:id', (request, response) => {
+
+    const queryText = "SELECT * FROM artists";
+
+    pool.query(queryText, (err, queryRes) => {
+        console.log(queryRes);
+
+        const artistsDB = queryRes.rows; //the array of objects from the database
+
+        const data = {
+            artists: artistsDB
+        };
+
+        response.render('showSingleArtist', data);
+
+    })
+});
+
 
 
 /**
@@ -68,16 +120,16 @@ app.get('/new', (request, response) => {
  */
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
-let onClose = function(){
-  
-  console.log("closing");
-  
-  server.close(() => {
-    
-    console.log('Process terminated');
-    
-    pool.end( () => console.log('Shut down db connection pool'));
-  })
+let onClose = function() {
+
+    console.log("closing");
+
+    server.close(() => {
+
+        console.log('Process terminated');
+
+        pool.end(() => console.log('Shut down db connection pool'));
+    })
 };
 
 process.on('SIGTERM', onClose);
