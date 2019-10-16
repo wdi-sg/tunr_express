6d46b7269fda9f65e0b18fd1216c15e3dc2125b7
David M <davidmosk@gmail.com>
2019-10-16 09:15:16 +0800

    fixed some end spacing

--- views/edit.jsx
+++ views/edit.jsx
@@ -9,13 +9,13 @@ class New extends React.Component {
                               <br/>
 
             <h1>Edit Artist: { this.props.name }</h1>
-            <form method="POST" action={"/artists/"+this.props.id+"?_method=put"}>
+            <form method="POST" action={"/artists/"+this.props.id+"?_method=put"} >
             <br/>
             Name: <input type="text" name="name" value={this.props.name}/>
             <br/>
             Image URL: <input type="text" name="photo_url" value={this.props.photo_url}/>
             <br/>
-            Nationality: <input type="text" name="nationality" value={this.props.nationality}/>
+            Nationality: <input type="text" name="nationality" value={ this.props.nationality }/>
             <br/>
             <input type="submit" value="Submit"/>
             </form>
