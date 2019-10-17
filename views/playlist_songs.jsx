88ccf41d52dc7cb6502a4b2ef7ff9a0bfd6a2321
David M <davidmosk@gmail.com>
2019-10-17 10:41:14 +0800

    fix display of songs, uses song_id

---
+++ views/playlist_songs.jsx
@@ -0,0 +1,43 @@
+var React = require("react");
+var DefaultLayout = require('./layouts/default');
+
+class PlaylistSongs extends React.Component {
+   render() {
+
+        let style = {
+            display: "inline-block",
+            margin: "2rem",
+            "text-align": "center"
+        }
+        let style2 = {
+            "text-decoration": "none"
+        }
+        const allSongs = this.props.songsobj.map( (songs, index) => {
+            return (
+                <li style={style}><a style={style2} href={"/song/"+songs.song_id} className="list-group-item list-group-item-action list-group-item-primary">{songs.title}</a>
+                <br/>
+                    <a href={"/song/"+songs.song_id+"/edit"} className="btn btn-sm btn-outline-info m-1">Edit</a>
+                    <a href={"/song/"+songs.song_id+"/delete"} className="btn btn-sm btn-outline-danger m-1">Delete</a>
+                </li>
+                )
+
+        })
+        return (
+
+          <DefaultLayout pageTitle={this.props.pageTitle}>
+
+
+
+                  { this.props.warning }
+                        <ul>
+                        {allSongs}
+                        </ul>
+
+
+          </DefaultLayout>
+        )
+    }
+}
+
+
+module.exports = PlaylistSongs;
