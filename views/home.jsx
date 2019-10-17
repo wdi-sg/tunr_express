24a2f5e8fc2b79d173a8ada31f4e61a53f97a99d
David M <davidmosk@gmail.com>
2019-10-17 13:08:31 +0800

    change to handle asc artist different than id

--- views/home.jsx
+++ views/home.jsx
@@ -14,7 +14,8 @@ class Home extends React.Component {
         }
         const allArtists = this.props.artistsobj.map( (artists, index) => {
             return (
-                <li style={style}><a style={style2} href={"/artists/"+(index+1)} className="list-group-item list-group-item-action list-group-item-primary">{artists.name}</a>
+                <li style={style}><img src={artists.photo_url} width="175px"/><br/>
+                <a style={style2} href={"/artists/"+(index+1)} className="list-group-item list-group-item-action list-group-item-primary">{artists.name}</a>
                 <br/>
                 <a href={"/artists/"+(index+1)+"/edit"} className="btn btn-sm btn-outline-info m-1">Edit</a>
                 <a href={"/artists/"+(index+1)+"/delete"} className="btn btn-sm btn-outline-danger m-1">Delete</a>
