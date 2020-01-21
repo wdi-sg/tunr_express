e5c5c7a2316166937d46e32cb77993d91337ea6c
Stuart Myers <stuart.myers@gmail.com>
2020-01-21 22:25:19 +0800

    fix mistaek

--- views/songs.jsx
+++ views/songs.jsx
@@ -6,7 +6,7 @@ class Songs extends React.Component {
       const songList = this.props.songs.map(song => {
         return <li><a href={song.preview_link}>{song.title}</a> - {song.album}</li>
       });
-        return (</DefaultLayout>
+        return (<DefaultLayout>
                 <h1>{this.props.artist.name} Songs:</h1>
                 <ul>{songList}</ul>
             </DefaultLayout>);
