642b26cff8ca758c8422cb9a747009346bbd5e60
leowzhenkang <leowzk@hotmail.com>
2020-01-21 18:18:44 +0800

    done mvp

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const pg = require('pg');
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
+  user: 'leowzhenkang',
   host: '127.0.0.1',
   database: 'tunr_db',
   port: 5432,
@@ -47,19 +47,56 @@ app.engine('jsx', reactEngine);
  * Routes
  * ===================================
  */
-
-app.get('/', (request, response) => {
-  // query database for all pokemon
-
-  // respond with HTML page displaying all pokemon
+//render home for app
+app.get('/tunr', (request, response) => {
   response.render('home');
 });
-
+//render form for adding new artists
 app.get('/new', (request, response) => {
-  // respond with HTML page with form to create new pokemon
   response.render('new');
 });
+//POST function for adding new artists
+app.post('/tunr', (request, response) => {
+    const queryString = 'INSERT INTO artists (name, photo_url, nationality) VALUES ($1, $2, $3)'
+    console.log(request.body)
 
+    const values = [
+        request.body.name,
+        request.body.photo_url,
+        request.body.nationality
+    ];
+    console.log(values)
+
+    pool.query(queryString, values, (err, result) => {
+
+        if (err) {
+            console.error('query error:', err.stack);
+            response.send('query error');
+        } else {
+            console.log('query result:', result);
+
+            // redirect to home page
+            response.send("Song Added!");
+        }
+    });
+});
+
+//for displaying list of all pokemon in table
+app.get('/artists/:id', (req, response) => {
+    // query database for all pokemon
+    const queryString = 'SELECT * from artists'
+
+    pool.query(queryString, (err, result) => {
+
+        if (err) {
+            console.error('query error:', err.stack);
+            response.send('query error');
+        } else {
+            console.log(req.params.id - 1)
+            response.send(result.rows[req.params.id - 1]);
+        }
+    });
+});
 
 /**
  * ===================================
@@ -69,13 +106,13 @@ app.get('/new', (request, response) => {
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
 let onClose = function(){
-  
+
   console.log("closing");
-  
+
   server.close(() => {
-    
+
     console.log('Process terminated');
-    
+
     pool.end( () => console.log('Shut down db connection pool'));
   })
 };
