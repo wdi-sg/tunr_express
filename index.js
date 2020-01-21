380a402503e4e32221091ea2a4604cb95b4cffc0
upieez <samuelhww@gmail.com>
2020-01-21 17:52:49 +0800

    inital commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("starting up!!");
+console.log("Loggin in to tunr_db database...");
 
 const express = require('express');
 const methodOverride = require('method-override');
@@ -6,16 +6,16 @@ const pg = require('pg');
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
-  host: '127.0.0.1',
-  database: 'tunr_db',
-  port: 5432,
+    user: 'samuelhuang',
+    host: '127.0.0.1',
+    database: 'tunr_db',
+    port: 5432,
 };
 
 const pool = new pg.Pool(configs);
 
 pool.on('error', function (err) {
-  console.log('idle client error', err.message, err.stack);
+    console.log('idle client error', err.message, err.stack);
 });
 
 /**
@@ -27,14 +27,12 @@ pool.on('error', function (err) {
 // Init express app
 const app = express();
 
-
 app.use(express.json());
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }));
 
 app.use(methodOverride('_method'));
-
 
 // Set react-views to be the default view engine
 const reactEngine = require('express-react-views').createEngine();
@@ -49,15 +47,15 @@ app.engine('jsx', reactEngine);
  */
 
 app.get('/', (request, response) => {
-  // query database for all pokemon
+    // query database for all pokemon
 
-  // respond with HTML page displaying all pokemon
-  response.render('home');
+    // respond with HTML page displaying all pokemon
+    response.render('home');
 });
 
 app.get('/new', (request, response) => {
-  // respond with HTML page with form to create new pokemon
-  response.render('new');
+    // respond with HTML page with form to create new pokemon
+    response.render('new');
 });
 
 
@@ -69,15 +67,15 @@ app.get('/new', (request, response) => {
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
 let onClose = function(){
-  
-  console.log("closing");
-  
-  server.close(() => {
-    
-    console.log('Process terminated');
-    
-    pool.end( () => console.log('Shut down db connection pool'));
-  })
+
+    console.log("closing");
+
+    server.close(() => {
+
+        console.log('Process terminated');
+
+        pool.end( () => console.log('Shut down db connection pool'));
+    })
 };
 
 process.on('SIGTERM', onClose);
