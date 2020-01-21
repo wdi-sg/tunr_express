2829c1951b8a481ea53a48e5040cbe731b10ff83
SafrazHakamali <safrazhakamali@gmail.com>
2020-01-21 20:57:05 +0800

    Completed set-up

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-console.log("starting up!!");
-
-const express = require('express');
-const methodOverride = require('method-override');
-const pg = require('pg');
-
-// Initialise postgres client
-const configs = {
-  user: 'YOURUSERNAME',
-  host: '127.0.0.1',
-  database: 'tunr_db',
-  port: 5432,
-};
-
-const pool = new pg.Pool(configs);
-
-pool.on('error', function (err) {
-  console.log('idle client error', err.message, err.stack);
-});
 
 /**
  * ===================================
@@ -24,19 +5,26 @@ pool.on('error', function (err) {
  * ===================================
  */
 
-// Init express app
+console.log("starting up!!");
+const express = require('express');
+const methodOverride = require('method-override');
+const pg = require('pg');
+const configs = {
+  user: 'safraz',
+  host: '127.0.0.1',
+  database: 'tunr_db',
+  port: 5432,
+};
+const pool = new pg.Pool(configs);
+pool.on('error', function (err) {
+  console.log('idle client error', err.message, err.stack);
+});
 const app = express();
-
-
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
-
 app.use(methodOverride('_method'));
-
-
-// Set react-views to be the default view engine
 const reactEngine = require('express-react-views').createEngine();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
@@ -50,7 +38,6 @@ app.engine('jsx', reactEngine);
 
 app.get('/', (request, response) => {
   // query database for all pokemon
-
   // respond with HTML page displaying all pokemon
   response.render('home');
 });
@@ -67,18 +54,19 @@ app.get('/new', (request, response) => {
  * ===================================
  */
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
-
 let onClose = function(){
-  
   console.log("closing");
-  
   server.close(() => {
-    
     console.log('Process terminated');
-    
     pool.end( () => console.log('Shut down db connection pool'));
   })
 };
 
 process.on('SIGTERM', onClose);
 process.on('SIGINT', onClose);
+
+
+//To create table & seed: 
+//psql -d tunr_db -U safraz -f tables.sql
+//psql -d tunr_db -U safraz -f artist_data.sql
+//psql -d tunr_db -U safraz -f songs.sql
