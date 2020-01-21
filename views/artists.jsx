304da1b647d913429127041a8bb05d9f6f627fe2
Stuart Myers <stuart.myers@gmail.com>
2020-01-21 23:04:00 +0800

    Artists list now a nice card view

--- views/artists.jsx
+++ views/artists.jsx
@@ -3,16 +3,23 @@ var DefaultLayout = require("./layouts/default");
 
 class Artists extends React.Component {
     render() {
-      const artistList = this.props.artists.map(artist => {
-        const artistLink = `/artists/${artist.id}`;
-        return <li><a href={artistLink}>{artist.name}</a> - <img src={artist.photo_url} width="128"/> - {artist.nationality}</li>
-      });
-        return (
-            <DefaultLayout title="Tunr">
-                <h1>Tunr Music</h1>
-                <h3>Artists:</h3>
-                <ul>{artistList}</ul>
-            </DefaultLayout>);
+        const artistList = this.props.artists.map(artist => {
+            const artistLink = `/artists/${artist.id}`;
+            return (<div className="card" style={{width: 18 + 'rem', display: 'inline-block', margin: 1 + 'em'}}>
+                <img src={artist.photo_url} className="card-img-top"/>
+                    <div className="card-body">
+                        <h5 className="card-title">{artist.name}</h5>
+                        <p className="card-text">This artist is from {artist.nationality}</p>
+                        <a href={artistLink} className="btn btn-primary">See more about {artist.name}</a>
+                    </div>
+                </div>) });
+
+              return (
+                <DefaultLayout title="Tunr">
+                    <h1>Tunr Music</h1>
+                    <h3>Artists:</h3>
+                    <ul>{artistList}</ul>
+                </DefaultLayout>);
     }
 }
 
