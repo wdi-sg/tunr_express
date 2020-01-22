54dd5858f46affe06606c209d3fb6a9331ca643a
Tsairenkun <Tsairenkun@gmail.com>
2020-01-22 09:10:34 +0800

    Add function (POST)

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const pg = require('pg');
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
+  user: 'tsairenkun',
   host: '127.0.0.1',
   database: 'tunr_db',
   port: 5432,
@@ -48,18 +48,30 @@ app.engine('jsx', reactEngine);
  * ===================================
  */
 
+//------ HOME PAGE-------------------
 app.get('/', (request, response) => {
-  // query database for all pokemon
-
-  // respond with HTML page displaying all pokemon
-  response.render('home');
+  let allquery = "SELECT * FROM artists";
+  pool.query(allquery, (err,result)=>{
+    var data = {artist:result.rows};
+    response.render('home', data)
+  })
 });
 
 app.get('/new', (request, response) => {
+
   // respond with HTML page with form to create new pokemon
   response.render('new');
 });
 
+app.post('/artists/new', (request,response)=>{
+
+    let newquery = 'INSERT into artists (name, photo_url,nationality) VALUES ($1, $2, $3)';
+
+  let values = [request.body.name, request.body.img, request.body.nationality]
+  pool.query(newquery, values, (err,result)=>{
+
+  })
+})
 
 /**
  * ===================================
@@ -69,13 +81,13 @@ app.get('/new', (request, response) => {
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
 let onClose = function(){
-  
+
   console.log("closing");
-  
+
   server.close(() => {
-    
+
     console.log('Process terminated');
-    
+
     pool.end( () => console.log('Shut down db connection pool'));
   })
 };
---
+++ views/layout.jsx
@@ -0,0 +1,43 @@
+var React = require('react');
+
+class layout extends React.Component {
+  render() {
+return(
+        <html>
+            <head>
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
+            </head>
+                <body>
+                    <header>
+                        <ul class="nav justify-content-center">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/">HOME</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">SORT</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <form class="form-inline my-2 my-lg-0" method = "POST" action = "/recipe/search?method=get">
+                              <input class="form-control mr-sm-2" type="search" placeholder="Search" name="search" aria-label="Search"/>
+                              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                            </form>
+                        </ul>
+                    </header>
+                    <div>
+                        <h1 class ="display-1 text-center">TUNR 1.0!</h1>
+                    </div>
+                    <div class = "container-fluid">
+                    {this.props.children}
+                    </div>
+                </body>
+        </html>
+        );
+    }
+}
+
+module.exports = layout;
