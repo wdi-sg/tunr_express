3c82341cb9f3c2953dc9a5181fe100a644db068f
joycemap <57893220+joycemap@users.noreply.github.com>
2020-01-22 09:11:22 +0800

    Added show and create functions

--- index.js
+++ index.js
@@ -1,84 +1,113 @@
 console.log("starting up!!");
-
 const express = require('express');
 const methodOverride = require('method-override');
 const pg = require('pg');
-
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
+  user: 'joyce',
   host: '127.0.0.1',
   database: 'tunr_db',
   port: 5432,
 };
-
 const pool = new pg.Pool(configs);
-
 pool.on('error', function (err) {
   console.log('idle client error', err.message, err.stack);
 });
-
 /**
  * ===================================
  * Configurations and set up
  * ===================================
  */
-
 // Init express app
 const app = express();
-
-
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
-
 app.use(methodOverride('_method'));
-
-
 // Set react-views to be the default view engine
 const reactEngine = require('express-react-views').createEngine();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', reactEngine);
-
 /**
  * ===================================
  * Routes
  * ===================================
  */
-
+/**
+ * ===================================
+ * HELLO WORLD
+ * ===================================
+ */
 app.get('/', (request, response) => {
   // query database for all pokemon
-
   // respond with HTML page displaying all pokemon
   response.render('home');
 });
 
-app.get('/new', (request, response) => {
-  // respond with HTML page with form to create new pokemon
+/**
+ * ===================================
+ * CREATE A NEW ARTIST 
+ * ===================================
+ */
+app.get('/artists/new', (request, response) => {
+  // respond with HTML page with form to create new artist
   response.render('new');
 });
+app.post('/artists', (request, response) => {
+  let insertQueryText = 'INSERT INTO artists (name, photo_url, nationality) VALUES ($1, $2, $3) RETURNING *';
+  const values = [
+    request.body.name,
+    request.body.photo_url,
+    request.body.nationality
+  ];
+  pool.query(insertQueryText, values, (err, result) => {
+    console.log("INSERT query callback")
+    console.log()
+    if (err) {
+      console.log("ERROR", err);
+      response.send("error")
+    } else {
+      console.log("DONE", result.rows)
+      response.send("Added artist" + request.body.name)
+    }
+  });
+})
 
+/**
+ * ===================================
+ * SHOW AN ARTIST 
+ * ===================================
+ */
+app.get("/artists/:id", (request, response) => {
+  let artistId = parseInt(request.params.id);
 
+  console.log(artistId);
+  let query = "SELECT * from artists where id =" + artistId;
+
+  pool.query(query, (err, result) => {
+    const data = {
+      id: result.rows[0].id,
+      name: result.rows[0].name,
+      photo_url: result.rows[0].photo_url,
+      nationality: result.rows[0].nationality
+    };
+    response.render('artistsSearch', data);
+  });
+});
 /**
  * ===================================
  * Listen to requests on port 3000
  * ===================================
  */
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
-
-let onClose = function(){
-  
+let onClose = function () {
   console.log("closing");
-  
   server.close(() => {
-    
     console.log('Process terminated');
-    
-    pool.end( () => console.log('Shut down db connection pool'));
+    pool.end(() => console.log('Shut down db connection pool'));
   })
 };
-
 process.on('SIGTERM', onClose);
 process.on('SIGINT', onClose);
--- views/new.jsx
+++ views/new.jsx
@@ -3,10 +3,58 @@ var React = require("react");
 class New extends React.Component {
   render() {
     return (
-      <html>
-        <head />
+      <html lang="en">
+        <head>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta httpequiv="X-UA-Compatible" content="ie=edge" />
+          <title>Create Recipe</title>
+          <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+            crossOrigin="anonymous"
+          />
+          <script
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+            crossOrigin="anonymous"
+          ></script>
+        </head>
         <body>
-          <h3>Form Goes Here!</h3>
+          <div
+            style={{ margin: `20px`, display: `flex`, flexDirection: `column` }}
+          >
+            <h1>Create a new artist!</h1>
+            <div style={{ marginBottom: `10px` }}></div>
+
+            <form action="/recipes" method="POST">
+              <div style={{ paddingBottom: `10px` }}>
+                <input type="text" placeholder="artist name" name="name" />
+              </div>
+
+              <div style={{ paddingBottom: `10px` }}>
+                <input type="text" placeholder="photo_url" name="photo_url" />
+              </div>
+              <div style={{ paddingBottom: `10px` }}>
+                <input
+                  type="text"
+                  placeholder="nationality"
+                  name="nationality"
+                />
+              </div>
+              <div>
+                <input
+                  type="submit"
+                  value="Create!"
+                  style={{ borderRadius: `5px`, marginRight: `10px` }}
+                />
+              </div>
+            </form>
+          </div>
         </body>
       </html>
     );
