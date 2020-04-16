9ca9ca4ee7ab05bc646380fcf940a53803063fae
Dyana Wu <dyanawu@gmail.com>
2020-04-16 17:22:05 +0800

    Create server scaffold

--- index.js
+++ index.js
@@ -1,84 +1,59 @@
-console.log("starting up!!");
-
+// init an express app
 const express = require('express');
-const methodOverride = require('method-override');
-const pg = require('pg');
-
-// Initialise postgres client
-const configs = {
-  user: 'YOURUSERNAME',
-  host: '127.0.0.1',
-  database: 'tunr_db',
-  port: 5432,
-};
-
-const pool = new pg.Pool(configs);
-
-pool.on('error', function (err) {
-  console.log('idle client error', err.message, err.stack);
-});
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
-// Init express app
 const app = express();
 
+// let forms override methods with "?_method=put"
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 
+// create and use react engine
+const reactEngine = require('express-react-views').createEngine();
+app.engine('jsx', reactEngine);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+
+// lets you do form parsing in req.body
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
 
-app.use(methodOverride('_method'));
-
-
-// Set react-views to be the default view engine
-const reactEngine = require('express-react-views').createEngine();
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jsx');
-app.engine('jsx', reactEngine);
-
-/**
- * ===================================
- * Routes
- * ===================================
- */
-
-app.get('/', (request, response) => {
-  // query database for all pokemon
-
-  // respond with HTML page displaying all pokemon
-  response.render('home');
-});
-
-app.get('/new', (request, response) => {
-  // respond with HTML page with form to create new pokemon
-  response.render('new');
-});
-
-
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
-const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
-
-let onClose = function(){
-  
-  console.log("closing");
-  
-  server.close(() => {
-    
-    console.log('Process terminated');
-    
-    pool.end( () => console.log('Shut down db connection pool'));
-  })
+// mount ./static onto virt path .pub
+app.use('/pub', express.static('./static'));
+let options = {
+  root: ('static'),
+  dotfiles: 'deny',
+  headers: {
+    'x-timestamp': Date.now(),
+    'x-sent': true
+  }
 };
 
-process.on('SIGTERM', onClose);
-process.on('SIGINT', onClose);
+// create postgres-node connection pool
+const pg = require('pg');
+const configs = {
+  user: 'dwu',
+  host: '127.0.0.1',
+  database: 'DATABASE',
+  port: 5432,
+};
+
+const pool = new pg.Pool(configs);
+pool.on('error', function (err) {
+  console.log('idle client error', err.message, err.stack);
+});
+
+// generic SQL query helper function
+const makeQuery = async function (query, values) {
+  let results = await pool.query(query, values);
+  return results.rows;
+};
+
+// routes
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+// start server listen
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
