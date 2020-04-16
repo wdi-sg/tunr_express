ac0c9ed9f03484cdcec3e3477698b648777eded0
Rachelik <rachel.ik.87@gmail.com>
2020-04-16 20:03:16 +0800

    IN: Set up express app done.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const pg = require('pg');
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
+  user: 'rachelik',
   host: '127.0.0.1',
   database: 'tunr_db',
   port: 5432,
@@ -69,13 +69,13 @@ app.get('/new', (request, response) => {
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
 let onClose = function(){
-  
+
   console.log("closing");
-  
+
   server.close(() => {
-    
+
     console.log('Process terminated');
-    
+
     pool.end( () => console.log('Shut down db connection pool'));
   })
 };
