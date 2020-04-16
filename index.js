98b69e30c59730da2011fc55f9ffa9a597439b1b
chelsejw <chelsejw@gmail.com>
2020-04-16 19:19:18 +0800

    basic setup and homepage with css

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const pg = require('pg');
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
-  host: '127.0.0.1',
-  database: 'tunr_db',
-  port: 5432,
+    user: 'chelseaee',
+    host: '127.0.0.1',
+    database: 'tunr_db',
+    port: 5432,
 };
 
 const pool = new pg.Pool(configs);
 
 pool.on('error', function (err) {
-  console.log('idle client error', err.message, err.stack);
+    console.log('idle client error', err.message, err.stack);
 });
 
 /**
@@ -30,8 +30,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }));
+app.use(express.static(__dirname + "/public/"));
+
 
 app.use(methodOverride('_method'));
 
@@ -48,16 +50,40 @@ app.engine('jsx', reactEngine);
  * ===================================
  */
 
-app.get('/', (request, response) => {
-  // query database for all pokemon
+app.get(`/artists`, (req, res)=> {
 
-  // respond with HTML page displaying all pokemon
-  response.render('home');
+  let command = `SELECT * FROM artists`
+
+  pool.query(command, (err,result) => {
+
+    if (err) {
+      console.log(`There was an error.`);
+      console.log(err.message)
+    } else {
+      const artistArr = result.rows;
+      const artistData = {
+        artists: artistArr
+      }
+
+      res.render('home', artistData)
+
+    }
+
+  })
+
+
+})
+
+app.get('/', (request, response) => {
+    // query database for all pokemon
+
+    // respond with HTML page displaying all pokemon
+    response.render('home');
 });
 
 app.get('/new', (request, response) => {
-  // respond with HTML page with form to create new pokemon
-  response.render('new');
+    // respond with HTML page with form to create new pokemon
+    response.render('new');
 });
 
 
@@ -68,16 +94,16 @@ app.get('/new', (request, response) => {
  */
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
-let onClose = function(){
-  
-  console.log("closing");
-  
-  server.close(() => {
-    
-    console.log('Process terminated');
-    
-    pool.end( () => console.log('Shut down db connection pool'));
-  })
+let onClose = function () {
+
+    console.log("closing");
+
+    server.close(() => {
+
+        console.log('Process terminated');
+
+        pool.end(() => console.log('Shut down db connection pool'));
+    })
 };
 
 process.on('SIGTERM', onClose);
--- views/home.jsx
+++ views/home.jsx
@@ -2,11 +2,32 @@ var React = require("react");
 
 class Home extends React.Component {
   render() {
+
+    const artistArr = this.props.artists;
+
+    const artistElements = artistArr.map ( artist=> {
+      return (
+        <div className="artist">
+          <h1>{artist.id}) {artist.name}</h1>
+          <img src={artist.photo_url}></img>
+        </div>
+      )
+    })
+
     return (
       <html>
-        <head />
+        <head>
+          <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+            crossOrigin="anonymous"
+          />
+          <link rel="stylesheet" href="/styles.css" />
+        </head>
         <body>
           <h1>Welcome!</h1>
+          {artistElements}
         </body>
       </html>
     );
