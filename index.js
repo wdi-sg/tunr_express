125da26b9a0a7e8fc23f702a4512d6df1e95ea8d
kenneththesheep <kenneththesheep@gmail.com>
2020-04-16 17:36:08 +0800

    Can view artist on one page

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const pg = require('pg');
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
+  user: 'kenneththesheep',
   host: '127.0.0.1',
   database: 'tunr_db',
   port: 5432,
@@ -52,7 +52,29 @@ app.get('/', (request, response) => {
   // query database for all pokemon
 
   // respond with HTML page displaying all pokemon
-  response.render('home');
+  response.send('hello world');
+});
+
+app.get('/artists',(request,response)=>{
+    const queryString = 'SELECT * from artists';
+
+pool.query(queryString, (err, result) => {
+
+  if (err) {
+    console.error('query error:', err.stack);
+    response.send( 'query error' );
+  } else {
+    //console.log('query result:', result);
+    const data={};
+
+    data.artist=result.rows;
+    // redirect to home page
+
+    response.render("home",data);
+    //response.send( data );
+  }
+});
+    //response.render('home');
 });
 
 app.get('/new', (request, response) => {
@@ -69,13 +91,13 @@ app.get('/new', (request, response) => {
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
 let onClose = function(){
-  
+
   console.log("closing");
-  
+
   server.close(() => {
-    
+
     console.log('Process terminated');
-    
+
     pool.end( () => console.log('Shut down db connection pool'));
   })
 };
--- views/home.jsx
+++ views/home.jsx
@@ -2,11 +2,40 @@ var React = require("react");
 
 class Home extends React.Component {
   render() {
+
+    const artistDetail=this.props.artist.map(artist=>
+        {console.log(artist);
+            const url="/artist/"+artist.id;
+            return <div class={"col-4 text-center border"}>
+            <div class={"row"} style={{height:"50%"}}>
+            <div class={"col-12"}>
+            <img style={{width: "100%", maxHeight:"70%"}} src={artist.photo_url}></img>
+            </div>
+            </div>
+            <div class={"row align-bottom"}>
+            <div class={"col-12 mt-4"}>
+            <p>Name: <a href={url}>{artist.name}</a></p>
+            <p>Nationality: {artist.nationality}</p>
+            </div>
+            </div>
+            </div>
+        });
+
     return (
       <html>
-        <head />
+        <head/>
+        <link rel={"stylesheet"} href={"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"} integrity={"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"} crossorigin={"anonymous"}></link>
         <body>
-          <h1>Hello World!</h1>
+        <div class={"container border mt-5"}>
+            <div class={"row text-center mt-3"}>
+                <div class={"col-12 text-center"}>
+                <h1>Artists</h1>
+                </div>
+            </div>
+            <div class="row">
+                {artistDetail}
+            </div>
+          </div>
         </body>
       </html>
     );
