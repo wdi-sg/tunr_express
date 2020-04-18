3627b7e9df453d98644a655328377b27ae7b89a9
Dyana Wu <dyanawu@gmail.com>
2020-04-18 15:22:07 +0800

    Add header jsx module

---
+++ views/head.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+const Head =
+        <head>
+          <link rel="stylesheet" href="/pub/css/bootstrap.min.css" />
+          <link rel="stylesheet" href="/pub/css/font-awesome.min.css" />
+          <link rel="stylesheet" href="/pub/css/bootstrap-checkbox-radio-list-group-item.min.css" />
+        </head>
+;
+
+module.exports = Head;
