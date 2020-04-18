2ac5702cac41a42656a7eb7d2ba9ba18f0ff067f
Dyana Wu <dyanawu@gmail.com>
2020-04-19 03:32:50 +0800

    Convert header module to class

--- views/head.jsx
+++ views/head.jsx
@@ -1,11 +1,15 @@
 const React = require('react');
 
-const Head =
-        <head>
-          <link rel="stylesheet" href="/pub/css/bootstrap.min.css" />
-          <link rel="stylesheet" href="/pub/css/font-awesome.min.css" />
-          <link rel="stylesheet" href="/pub/css/bootstrap-checkbox-radio-list-group-item.min.css" />
-        </head>
-;
+class Head extends React.Component {
+  render() {
+    return (
+      <head>
+        <link rel="stylesheet" href="/pub/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="/pub/css/font-awesome.min.css" />
+        <link rel="stylesheet" href="/pub/css/bootstrap-checkbox-radio-list-group-item.min.css" />
+      </head>
+    );
+  }
+}
 
 module.exports = Head;
