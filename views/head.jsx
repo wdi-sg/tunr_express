5968655c1180f1f32b554dab703464613484bb24
Dyana Wu <dyanawu@gmail.com>
2020-04-19 06:49:49 +0800

    Change paths to reflect new mount point for static files

--- views/head.jsx
+++ views/head.jsx
@@ -4,9 +4,9 @@ class Head extends React.Component {
   render() {
     return (
       <head>
-        <link rel="stylesheet" href="/pub/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="/pub/css/font-awesome.min.css" />
-        <link rel="stylesheet" href="/pub/css/bootstrap-checkbox-radio-list-group-item.min.css" />
+        <link rel="stylesheet" href="/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="/css/font-awesome.min.css" />
+        <link rel="stylesheet" href="/css/bootstrap-checkbox-radio-list-group-item.min.css" />
       </head>
     );
   }
