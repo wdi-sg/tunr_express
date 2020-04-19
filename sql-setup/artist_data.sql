c8630c06abab1b222c247ecc240332584985271e
Dyana Wu <dyanawu@gmail.com>
2020-04-19 16:10:58 +0800

    Modify basic artist data

--- sql-setup/artist_data.sql
+++ sql-setup/artist_data.sql
@@ -1,5 +1,5 @@
-INSERT INTO artists(name, photo_url, nationality) VALUES('Yeah Yeah Yeahs', 'http://www.athousandguitars.com/wp-content/uploads/2013/04/yeah-yeah-yeahs.jpg', 'USA');
+INSERT INTO artists(name, photo_url, nationality) VALUES('Yeah Yeah Yeahs', 'https://www.nme.com/wp-content/uploads/2018/05/Yeah-Yeah-Yeahs-696x442.jpg', 'USA');
 INSERT INTO artists(name, photo_url, nationality) VALUES('Nosaj Thing', 'http://wertn.com/wp-content/uploads/2012/04/Nosaj-Thing_Mondrian_CL_High-3487.jpg', 'USA');
 INSERT INTO artists(name, photo_url, nationality) VALUES('Norah Jones', 'http://entertainmentrealm.files.wordpress.com/2012/05/norahjones1.jpg', 'USA');
-INSERT INTO artists(name, photo_url, nationality) VALUES('Lykke Li', 'http://www.chartattack.com/wp-content/uploads/2012/07/lykke-li-newmain1-photo-by-daniel-jackson.jpg', 'Sweeden');
-INSERT INTO artists(name, photo_url, nationality) VALUES('Kendrick Lamar', 'http://www.xxlmag.com/wp-content/uploads/2013/06/kendricklamar_001-1600.jpg', 'USA');
+INSERT INTO artists(name, photo_url, nationality) VALUES('Lykke Li', 'https://www.yenmag.net/wp-content/uploads/2012/02/LL011.jpg', 'Sweden');
+INSERT INTO artists(name, photo_url, nationality) VALUES('Kendrick Lamar', 'https://compote.slate.com/images/d9a99820-5841-4b90-bc20-cb3b86af7f65.jpg', 'USA');
