43a67b424c84e9660fc840eaac167511185e40de
mdazharaw <darkaz62@hotmail.com>
2020-04-20 10:43:39 +0800

    Fixed navbar toggle

--- views/artist-songs.jsx
+++ views/artist-songs.jsx
@@ -38,14 +38,14 @@ class ArtistSongs extends React.Component {
     songs = songs.map((element) => {
       return <li key={element.title} className="list-group-item list-group-item-action"><a href={`${element.preview_link}`} target="_blank">
         <img style={square2} src={`${element.artwork}`} className="float-left mr-3" />
-        <h6 className = " text-dark"> <strong>{element.songid}. {element.title}</strong> </h6>
-        <h7 className = " text-dark"><strong>Album: </strong>{element.album}</h7>
-        </a>
+        <h6 className=" text-dark"> <strong>{element.songid}. {element.title}</strong> </h6>
+        <h7 className=" text-dark"><strong>Album: </strong>{element.album}</h7>
+      </a>
         <a href={`songs/edit/${element.id}`} className="btn btn-warning float-right mr-3">Edit</a>
         <a href={`/playlists/${element.id}/newsong`} className="btn btn-success text-light  float-right mr-3">Add to Playlist</a>
         <br />
       </li>
-      
+
     })
     if (songs.length == 0) {
       songs = [
@@ -64,7 +64,7 @@ class ArtistSongs extends React.Component {
 
         </head>
         <body className="bg-secondary">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="/artists">Tunr Express</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
@@ -86,13 +86,16 @@ class ArtistSongs extends React.Component {
               <a key="add-new" href="songs/new"><li className="list-group-item list-group-item-action">
                 <img style={square2} src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRybEoxX-zFDrhiWh8S9UB-ij1uf8jNp87KlIG-g04MYjtKvKMU&usqp=CAU" className="float-left mr-3" />
                 <h6 className="pt-3 align-text-bottom">Add New Song</h6>
-                
+
                 <br />
               </li>
               </a>
               {songs}
             </ul>
           </div>
+          <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
+          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
         </body>
       </html>
     );
--- views/artists.jsx
+++ views/artists.jsx
@@ -59,7 +59,7 @@ class Artists extends React.Component {
               </div>
             </div>
           </nav>
-          
+
           <div className="container-fluid">
             <h1 className="text-center w-75 ml-auto mr-auto bg-dark text-light"><em><u>Artists</u></em></h1>
             <div className="row row-cols-3 w-75 ml-auto mr-auto">
@@ -74,6 +74,9 @@ class Artists extends React.Component {
               </a>
             </div>
           </div>
+          <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
+          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
         </body>
       </html>
     );
