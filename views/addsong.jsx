183e57da7fc1fd9d526a8e3805e2ddae1ffe7739
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-04-21 00:40:49 +0800

    added external stylesheet to playlist/id/newsong. Yet to add for other pages

--- views/addsong.jsx
+++ views/addsong.jsx
@@ -59,10 +59,11 @@ class Addsong extends React.Component {
         <head>
           <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"/>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css"/>
+          <link rel="stylesheet" href="/addsong.css"/>
         </head>
         <body>
           <h1>Add New Song</h1>
-          <div>
+          <div className="visits">
             {visitbadge}
           </div>
           <div>
