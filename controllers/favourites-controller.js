3e023d1aa8ff61a9555cf64da640335164a0ab51
Zachariah Chow <zachariahchow@gmail.com>
2020-04-22 01:20:04 +0800

    added postFavourites controller

--- controllers/favourites-controller.js
+++ controllers/favourites-controller.js
@@ -2,7 +2,28 @@ const db = require('../util/database.js');
 
 module.exports.postFavourites = async (req, res) => {
 
-    console.log(req.body);
-    // const queryT1 = `SELECT * FROM users WHERE email ='${email}'`
-    // const { rows } = await db.query(queryT1)
+    const queryT1 = `SELECT * from FAVOURITES where user_id=${req.currentUser.id}`;
+
+    const { rows } = await db.query(queryT1);
+
+    console.log(rows);
+
+    const songAlrInFavs = rows.some(log => {
+        return log['song_id'] == req.body.songId;
+    })
+
+    if (songAlrInFavs || !rows[0]) {
+
+        return;
+
+    } else {
+
+        const queryT2 = `INSERT INTO favourites (user_id, song_id) VALUES($1, $2)`;
+        const queryV2 = [req.currentUser.id, req.body.songId];
+
+        const resultTwo = await db.query(queryT2, queryV2);
+
+        console.log(resultTwo['rows']);
+
+    }
 }
