a407d7906596166f515240b09751c9de0fa8a5d6
Dyana Wu <dyanawu@gmail.com>
2020-04-21 22:26:56 +0800

    Shift counter to bottom right

--- views/foot.jsx
+++ views/foot.jsx
@@ -3,7 +3,7 @@ const React = require('react');
 class Foot extends React.Component {
   render() {
     return (
-      <footer className="footer mt-auto bg-info">
+      <footer className="footer mt-auto bg-info text-right">
         <p className="px-3 py-2 m-0 text-white"> Visits: {this.props.count}</p>
       </footer>
     );
