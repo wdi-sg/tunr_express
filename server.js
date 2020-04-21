ff6becf8a6a34ce9d4f1708affe4ed95b646ab38
Kenan <xinkenan.11@hotmail.com>
2020-04-21 10:32:43 +0800

    add cookie parser

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.user('cookie-parser')
 
 const run = async () => {
 
