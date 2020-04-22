9e4aa566e69ecd472b4a025c3f235960235c8684
Dyana Wu <dyanawu@gmail.com>
2020-04-22 08:06:48 +0800

    Show different navbar depending on user authed

--- views/nav.jsx
+++ views/nav.jsx
@@ -2,14 +2,29 @@ const React = require('react');
 
 class Nav extends React.Component {
   render() {
-    return (
-      <nav className="nav nav-pills nav-justified list-group list-group-horizontal">
-        <a className="nav-item nav-link list-group-item-action text-info h4" href="/">Home</a>
-        <a className="nav-item nav-link list-group-item-action text-info h4" href="/artists">Artists</a>
-        <a className="nav-item nav-link list-group-item-action text-info h4" href="/songs">Songs</a>
-        <a className="nav-item nav-link list-group-item-action text-info h4" href="/playlists">Playlists</a>
+    let loginout =
+      <a className="nav-item nav-link list-group-item-action text-info"
+         href="/login">Login</a>;
+    let register =
+      <a className="nav-item nav-link list-group-item-action text-info"
+         href="/register">Register</a>
+      if (this.props.auth) {
+        loginout =
+          <a className="nav-item nav-link list-group-item-action text-info"
+             href="/logout">Logout</a>;
+        register = "";
+      }
+
+      return (
+      <nav className="navbar nav-pills nav-justified list-group list-group-horizontal bg-light">
+        <a className="nav-item nav-link list-group-item-action text-info" href="/">Home</a>
+        <a className="nav-item nav-link list-group-item-action text-info" href="/artists">Artists</a>
+        <a className="nav-item nav-link list-group-item-action text-info" href="/songs">Songs</a>
+        <a className="nav-item nav-link list-group-item-action text-info" href="/playlists">Playlists</a>
+        {register}
+        {loginout}
       </nav>
-    );
+      );
   }
 }
 
