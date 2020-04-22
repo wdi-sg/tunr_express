5d4bf0d43e77cfa5bc03011123832c27a0cc7d2c
Dyana Wu <dyanawu@gmail.com>
2020-04-22 08:04:52 +0800

    Tweak footer to show username

--- views/foot.jsx
+++ views/foot.jsx
@@ -2,9 +2,10 @@ const React = require('react');
 
 class Foot extends React.Component {
   render() {
+    let username = this.props.data.user || "";
     return (
       <footer className="footer mt-auto bg-info text-right">
-        <p className="px-3 py-2 m-0 text-white"> Visits: {this.props.count}</p>
+        <p className="px-3 py-2 m-0 text-white">{username} Visits: {this.props.data.count}</p>
       </footer>
     );
   }
