14c519263f1ba8e04278ae9b1e21e28ea8642269
Dyana Wu <dyanawu@gmail.com>
2020-04-22 08:18:37 +0800

    Add user registration and log in

--- modules/routes.js
+++ modules/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // user auth
 const hash = require('node_hash');
-const makeRandString = require('./crypt.js');
+const rsP = require('./crypt.js');
 
 // helper functions
 const makeQuery = require('./makequery');
@@ -29,8 +29,37 @@ router.get('/register', (req, res) => {
   res.render('userform', data);
 });
 
-router.post('/register', (req, res) => {
-//  res.render('registerform', {sitecount: req.visitCount});
+router.post('/register', async (req, res) => {
+  let form = req.body;
+  let userCheckQuery = "SELECT * FROM users WHERE username = $1";
+  let existUsers = await makeQuery(userCheckQuery, [form.username]);
+  if (existUsers.length > 0) {
+    let data = {
+      title: "Register for Tunr",
+      button: "Register",
+      action: "/register",
+      sitecount: req.visitCount,
+      error: "That username is taken, please choose another."
+    };
+    res.render('userform', data);
+    return;
+  }
+
+  let salt = await rsP(4);
+  let saltedPass = hash.sha256(form.password, salt);
+  let values = [
+    form.username,
+    saltedPass,
+    salt
+  ];
+
+  let userQuery =
+      "INSERT INTO users " +
+      "(username, password, salt) " +
+      "VALUES ($1, $2, $3)";
+
+  await makeQuery(userQuery, values);
+  res.redirect('/');
 });
 
 router.get('/login', (req, res) => {
@@ -44,8 +73,42 @@ router.get('/login', (req, res) => {
   res.render('userform', data);
 });
 
-router.post('/login', (req, res) => {
-//  res.render('loginform', {sitecount: req.visitCount});
+router.post('/login', async (req, res) => {
+  let form = req.body;
+  let authQuery = "SELECT * FROM users WHERE username = $1";
+  let authResults = await makeQuery(authQuery, [form.username]);
+  let saltedPass = hash.sha256(form.password, authResults[0].salt);
+
+  if (authResults.length > 0 && authResults[0].password === saltedPass) {
+    let session = await rsP(30);
+    let sesId = hash.sha256(session);
+
+    let sessionQuery =
+        "INSERT INTO sessions " +
+        "(id, user_id) " +
+        "VALUES ($1, $2)";
+    let sessionValues = [
+      sesId,
+      authResults[0].id
+    ];
+    await makeQuery(sessionQuery, sessionValues);
+    res.cookie('session', sesId);
+    res.redirect('/');
+  }
+
+  let data = {
+    title: "Log In to Tunr",
+    button: "Log In",
+    action: "/login",
+    sitecount: req.visitCount,
+    error: "Unable to log in"
+  };
+  res.render('userform', data);
+});
+
+router.get('/logout', (req, res) => {
+  res.clearCookie('session');
+  res.redirect('/');
 });
 
 router.get('/testpage', (req, res) => {
