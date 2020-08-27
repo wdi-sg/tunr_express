60b22e3829a290fe7891c3feff4b6fb36a8db70c
exhlim <exhlimm@gmail.com>
2020-08-27 23:02:53 +0800

    Feature to see all the artist

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const pg = require('pg');
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
+  user: 'eugenelim',
   host: '127.0.0.1',
   database: 'tunr_db',
   port: 5432,
@@ -47,18 +47,48 @@ app.engine('jsx', reactEngine);
  * Routes
  * ===================================
  */
-
+let page;
 app.get('/', (request, response) => {
   // query database for all pokemon
-
+  response.send("Hello world");
   // respond with HTML page displaying all pokemon
-  response.render('home');
+  // response.render('home');
 });
+
+app.get('/artists', (request , response) => {
+    let queryShow = "SELECT * FROM Artists"
+    pool.query(queryShow, (err, res) => {
+        if(err){console.log(err);}
+        let artistList = []
+        res.rows.forEach(item => {
+            artistList += (`<li> ${item.name} </li> <br>`)
+        })
+        response.send(`<h1>Artists</h1> <ol>${artistList}</ol>`);
+    })
+})
+
+
+
+
+
+
+
 
 app.get('/new', (request, response) => {
   // respond with HTML page with form to create new pokemon
   response.render('new');
 });
+
+
+// const displayArtists=(req)=> {
+//     pool.query(req, (err, response) => {
+//     console.log(response);
+//     return response;
+//     })
+// }
+
+
+
 
 
 /**
@@ -69,13 +99,13 @@ app.get('/new', (request, response) => {
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
 let onClose = function(){
-  
+
   console.log("closing");
-  
+
   server.close(() => {
-    
+
     console.log('Process terminated');
-    
+
     pool.end( () => console.log('Shut down db connection pool'));
   })
 };
